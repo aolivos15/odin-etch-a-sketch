f103c95554d9ea46cb293f7af1f54330cbf9d48d
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-07-29 16:00:00 -0400

    Add resize grid button

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 const defaultSize = 16;
 
+const grid = document.querySelector(".grid-container");
+const resizeBtn = document.querySelector("#resize-btn");
+const resizeInput = document.querySelector("#resize-input");
+
 const colorSquare = (square) => {
   square.style.backgroundColor = "turquoise";
 }
@@ -8,13 +12,20 @@ const clearSquare = (square) => {
   square.style.backgroundColor = "white";
 }
 
-const grid = document.querySelector(".grid-container");
 
 const createGrid = (size) => {
-  for (let i = 0; i < (size-1)*(size-1); i++) {
+  for (let i = 1; i < (size)*(size)+1; i++) {
     const square = document.createElement("div");
-    square.style.width = `calc(100% / ${size})`;
+    square.style.width = `calc(640px / ${size})`;
     square.classList.add("square");
+    // Add right border to last element of each row
+    if (i % size == 0) {
+      square.classList.add("border-right");
+    }
+    // Add bottom border to all elements of last row
+    if (i > (size*(size-1))) {
+      square.classList.add("border-bottom");
+    }
 
     const controller = new AbortController();
 
@@ -29,5 +40,24 @@ const createGrid = (size) => {
     grid.appendChild(square);
   }
 }
+
+
+const resizeGrid = () => {
+  const size = resizeInput.value;
+  if (!size || size < 0) {
+    alert("Please enter a valid size for the new grid.");
+    return;
+  } else if (size > 100) {
+    alert("The size of the grid can't be greater than 100.")
+    return;
+  }
+  grid.replaceChildren();
+  createGrid(size);
+}
+
+resizeBtn.addEventListener("click", () => {
+  resizeGrid()
+});
+
 
 createGrid(defaultSize);
